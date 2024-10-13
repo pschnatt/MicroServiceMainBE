e70f6d28ba24647fabb9a7f94fe943d0111199bb
HariTTira <haritpao@gmail.com>
2024-10-13 21:21:30 +0700

    add CORS

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from apiGateway.src.routes.restaurant import router as restaurantrouter
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(restaurantrouter, prefix="/api/restaurant")
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+
+
