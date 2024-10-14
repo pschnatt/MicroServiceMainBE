38df88cb4b14f2390559f5b52131a1367e222d96
shinyyxxx <puriwat888@gmail.com>
2024-10-15 01:07:19 +0700

    fix : add bookingApi to router

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from apiGateway.src.routes.restaurant import router as restaurantrouter
 from apiGateway.src.routes.user import router as userrouter
 from apiGateway.src.routes.payment import router as paymentrouter
+from apiGateway.src.routes.booking import router as bookingrouter
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -18,6 +19,7 @@ app.add_middleware(
 app.include_router(restaurantrouter, prefix="/api/restaurant")
 app.include_router(userrouter, prefix="/api/user")
 app.include_router(paymentrouter, prefix="/api/payment")
+app.include_router(bookingrouter, prefix="/api/booking")
 
 if __name__ == "__main__":
     import uvicorn
