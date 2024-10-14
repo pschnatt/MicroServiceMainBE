98005caa8511410d498936f05019a0c1323b1a6a
Soe Moe Stephen <soe@frontware.co.th>
2024-10-14 16:10:26 +0700

    feat: payment api added in main BE

--- apiGateway/src/config.py
+++ apiGateway/src/config.py
@@ -1,3 +1,4 @@
 RESTAURANT_API_URL = "http://127.0.0.1:8000/api/restaurant"
 BOOKING_API_URL = "http://127.0.0.1:8001/api/booking"
 USER_API_URL = "http://localhost:8002/api/user"
+PAYMENT_API_URL = "http://127.0.0.1:8003/api/payment"
---
+++ apiGateway/src/routes/payment.py
@@ -0,0 +1,61 @@
+from fastapi import APIRouter, HTTPException
+import httpx
+from apiGateway.src.config import PAYMENT_API_URL
+
+router = APIRouter()
+
+@router.post("/{userId}/create")
+async def create_payment(userId: str, payment_data: dict):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{PAYMENT_API_URL}/{userId}/create", json=payment_data)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.get("/payments")
+async def retrieve_all_payments():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{PAYMENT_API_URL}/payments",  timeout=100.0)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.get("/payments/{userId}")
+async def retrieve_all_payments_ByUserId(userId: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{PAYMENT_API_URL}/payments/{userId}",  timeout=100.0)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.get("/payments/singlePayment/{paymentId}")
+async def retrieve_single_payment_ByUserId(paymentId: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{PAYMENT_API_URL}/payments/singlePayment/{paymentId}",  timeout=100.0)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.get("/account/{paymentId}")
+async def retrieve_account_ByPaymentId(paymentId: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{PAYMENT_API_URL}/account/{paymentId}")
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.put("/{paymentId}/update/")
+async def update_restaurant(paymentId: str, payment_data: dict):
+    async with httpx.AsyncClient() as client:
+        response = await client.put(f"{PAYMENT_API_URL}/{paymentId}/update/", json=payment_data)
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+@router.delete("/{userId}/delete/{paymentId}")
+async def delete_restaurant(userId: str, paymentId: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.delete(f"{PAYMENT_API_URL}/{userId}/delete/{paymentId}")
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
