ae457bad54054937525a17680d832b3e1f8a47e4
Soe Moe Stephen <soe@frontware.co.th>
2024-10-15 13:25:44 +0700

    fix: docker file naming fixed

--- apiGateway/src/config.py
+++ apiGateway/src/config.py
@@ -1,4 +1,4 @@
 RESTAURANT_API_URL = "http://microservice_restaurant_api:8000/api/restaurant"
-BOOKING_API_URL = "http://mircroservice_booking_api:8001/api/booking"
+BOOKING_API_URL = "http://microservice_booking_api:8001/api/booking"
 USER_API_URL = "http://microservice_user_api:8002/api/user"
 PAYMENT_API_URL = "http://microservice_payment_api:8003/api/payment"
